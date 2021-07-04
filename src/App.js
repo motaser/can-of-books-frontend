d5aadc91cce2dfd8d88c807949bba5c502d0c0f7
Osamamomani1 <momaniosama97@gmail.com>
2021-07-04 18:31:33 +0300

    osama

--- src/App.js
+++ src/App.js
@@ -8,24 +8,34 @@ import {
   Route
 } from "react-router-dom";
 import { withAuth0 } from '@auth0/auth0-react';
-import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 import Profile from './Profile';
 import BestBooks from './BestBooks'
+import Login from './Login';
+
+
 class App extends React.Component {
 
   render() {
-    console.log('app', this.props);
+    console.log('app', this.props.auth0);
     return(
       <>
         <Router>
+          
           <IsLoadingAndError>
             <Header />
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                { this.props.auth0.isAuthenticated ?
+                <>
+                <BestBooks/>
+                </> :
+                <Login/>
+                    }
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+
             </Switch>
             <Footer />
           </IsLoadingAndError>
@@ -35,4 +45,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0(App);
--- src/Profile.js
+++ src/Profile.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import { withAuth0 } from '@auth0/auth0-react';
+import { Card } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
  class Profile extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={this.props.auth0.user.picture} alt={this.props.auth0.user.name} />
+            <Card.Body>
+            <Card.Title>{this.props.auth0.user.name}</Card.Title>
+            <Card.Text>
+            {this.props.auth0.user.email}
+            </Card.Text>
+            </Card.Body>
+            </Card>
         )
     }
 }
 
-export default Profile
+export default withAuth0(Profile);
